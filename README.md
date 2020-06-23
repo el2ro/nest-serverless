f8305de33262a2da681df2da4a62baaca326fb10
Sami Torola <sami.torola@gmail.com>
2020-06-23 15:39:17 +0300

    Changed to show aws-lambda-fastify WS issue

--- README.md
+++ README.md
@@ -1,48 +1,32 @@
-# Simple NestJS Serverless example
-Very simple serverless "Hello world" type example using NestJS, Serverless framework and AWS lambda function
+# AWS-Lambda-fastify & NestJS Serverless example
+
+This is used to show problem with Websocket Api communication
 
 ## Installation
 
 ```bash
-yarn install
+npm install
 ```
 
-## Running the app
+## Build & Running the app
 
-Default Nest server in watch mode:
- ```bash
-yarn run start:dev
+```bash
+npm build
 ```
-If all goes well the app should be available on `3000` port:
+
+Launch the serverless offline
+
+```
+npm run start:sls
+```
+
+If all goes well the API Endpoint should be available on `3000` port and WS should be available on port `3001`
 
 http://localhost:3000
+ws://localhost:3001
 
-Serverless offline plugin:
-```bash
-yarn run start:sls
-```
+To test those
 
-Testing the app with serverless `invoke local`:
-```bash
-yarn run test:sls
-```
+curl http://localhost:3000/dev/test
 
-Packaging and deploying the app:
-```bash
-yarn run deploy
-```
-
-Only package: 
-```bash
-yarn run package
-```
-
-## Changed files
-- app.ts
-- main.ts
-- lambda.ts
-
-## Swagger
-When running the app locally, swagger documentation is available under `/docs` path:
-
-http://localhost:3000/docs
+wscat -c ws://localhost:3001
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,20 +1,70 @@
+import { NestFactory } from '@nestjs/core';
 import {
-    Context,
-    APIGatewayProxyEvent,
-    APIGatewayProxyResult,
-    Handler
+  //APIGatewayEvent,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  //Callback,
+  Context,
+  Handler,
 } from 'aws-lambda';
-import {proxy} from 'aws-serverless-express';
-import {bootstrap, NestServer} from './app';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import * as awsLambdaFastify from 'aws-lambda-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as fastify from 'fastify';
 
-let cachedServer: NestServer;
+import { AppModule } from './app.module';
 
-// noinspection JSUnusedGlobalSymbols
-export const handler: Handler =
-    async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-        if (!cachedServer) {
-            cachedServer = await bootstrap();
-        }
+interface NestApp {
+  app: NestFastifyApplication;
+  instance: fastify.FastifyInstance;
+}
 
-        return proxy(cachedServer.expressServer, event, context, 'PROMISE').promise;
-    };
+export async function bootstrap(): Promise<NestApp> {
+  const serverOptions: fastify.ServerOptionsAsHttp = {
+    logger: true,
+  };
+  const instance: fastify.FastifyInstance = fastify(serverOptions);
+  // env = process.env;
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(instance),
+    {
+      logger: 'local' === process.env['APP_STAGE'] ? new Logger() : console,
+    },
+  );
+
+  // app.use(eventContext());
+  // app.enableCors();
+  // app.setGlobalPrefix('v1');
+  // app.useGlobalPipes(new ValidationPipe());
+
+  //if (!isLocal) {
+  // Following are only needed when running in AWS lambda function
+  await app.init();
+  //}
+
+  return {
+    app,
+    instance,
+  };
+  // Lambda should have shutdown
+  // await app.close();
+}
+
+let cachedNestApp: NestApp;
+const binaryMimeTypes: string[] = [];
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> => {
+  if (!cachedNestApp) {
+    cachedNestApp = await bootstrap();
+  }
+
+  const proxy = awsLambdaFastify(cachedNestApp.instance);
+  return proxy(event, context /*, binaryMimeTypes*/);
+};
